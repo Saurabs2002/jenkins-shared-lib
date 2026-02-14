fb935f24f2677c4ffab32eefe874b65f36e402be
Saurabs2002 <129584008+Saurabs2002@users.noreply.github.com>
2026-02-14 09:25:20 +0000

    Refactor push function for better parameterization
    Refactor push function to use parameters for project and image tag, and improve credential handling.

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,14 +1,17 @@
-def call(String Project, String Image, String dockeUser){
-  withCredentials([usernamePassword(
-            credentialsId: 'docker-hub',
-            usernameVariable: 'DOCKER_USER',
-            passwordVariable: 'DOCKER_PASS'
-        )]) {
+def call(String project, String imageTag) {
 
-            sh '''
-                echo $DOCKER_PASS | docker login -u ${dockerUser} --password-stdin
-                docker tag ${Project}:${Image} ${dockerUser}/${Project}:${Image}
-                docker push ${dockerUser}/${Project}:${Image}
-            '''
-        }
+    withCredentials([usernamePassword(
+        credentialsId: 'docker-hub',
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+
+        sh """
+            echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
+            docker tag ${project}:${imageTag} \$DOCKER_USER/${project}:${imageTag}
+            docker push \$DOCKER_USER/${project}:${imageTag}
+        """
+    }
+
+    echo "Docker image pushed successfully!"
 }
